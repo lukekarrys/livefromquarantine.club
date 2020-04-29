0cf311d788b7fdde48d8566d26e49b216996c64a
Luke Karrys <lukekarrys@gmail.com>
2020-04-28 17:27:55 -0700

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+
+## Links
+- [AJJ Playlist](https://www.youtube.com/playlist?list=PLRSI_QNxGZ2lZP141po9tLGpLqM6ciuP1)
+- [Docs to get youtube playlist videos](https://developers.google.com/youtube/v3/docs/playlistItems/list)
+
+## Get Data
+Run this to get `raw.json`:
+
+```sh
+curl \
+  'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLRSI_QNxGZ2lZP141po9tLGpLqM6ciuP1&key=[YOUR_API_KEY]' \
+  --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+  --header 'Accept: application/json' \
+  --compressed
+```
+
+Then run `node parse-data.js` which saves data to `parsed.js`
---
+++ parse-data.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const raw = require('./raw.json')
+
+const data = raw.items.map((r) => {
+  const {title, publishedAt, description, resourceId} = r.snippet
+  const hasTimestamp = /\d+:\d+/.test(r.snippet.description)
+  let songs = null
+
+  if (hasTimestamp) {
+    const descWithSet = description.split(/7:30(?: PM)?(?: (?:Pacific|PST))/i)[1] || description
+    const descLines = descWithSet.split('\n')
+    const songLines = descLines.filter((line) => /\d+:\d+/.test(line))
+    songs = songLines.map((line) => {
+      const [match, start, __, end] = line.match(/(\d+:\d+)( ?- ?(\d+:\d+))?/)
+      const name = line.replace(match, '')
+      return {
+        name: name.trim().replace(/^-/, '').replace(/-$/, '').trim(),
+        time: {
+          start,
+          end
+        }
+      }
+    })
+  }
+
+  return {
+    title,
+    publishedAt,
+    description,
+    resourceId,
+    songs
+  }
+}).sort((a, b) => a.publishedAt - b.publishedAt)
+
+fs.writeFileSync('./parsed.js', `window.__DATA=${JSON.stringify(data)}`)
