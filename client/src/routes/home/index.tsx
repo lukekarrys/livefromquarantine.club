57203756a11c1ea48d90de0fe8d130428832475b
Luke Karrys <lukekarrys@gmail.com>
2020-05-18 11:47:25 -0700

    main buttons

--- client/src/routes/home/index.tsx
+++ client/src/routes/home/index.tsx
@@ -4,13 +4,13 @@ import Button from "../../components/button"
 
 const Home: FunctionalComponent = () => {
   return (
-    <div class="mt-4 max-w-screen-md mx-auto px-2 md:px-0">
+    <div class="mt-4 max-w-screen-md mx-auto px-2">
       {config.artists.map((artist) => (
         <Button
           as="a"
           key={artist.id}
           href={`/${artist.id}`}
-          class="w-full block text-center mb-4 text-4xl uppercase"
+          class="w-full block text-center mb-4 text-4xl"
         >
           {artist.name}
         </Button>
