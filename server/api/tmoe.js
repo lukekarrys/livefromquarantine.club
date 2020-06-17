b76c5447787f58209685170f631e75a4461eb0c4
Luke Karrys <lukekarrys@gmail.com>
2020-06-16 23:16:16 -0700

    add tmoe

--- server/api/tmoe.js
+++ server/api/tmoe.js
@@ -1,7 +1,14 @@
-module.exports.parsers = {}
+module.exports.parsers = {
+  title: (title) =>
+    title
+      .replace(/The Tallest Man on Earth - /i, '')
+      .replace(/#StayHome/i, '')
+      .replace(/#WithMe/i, '')
+      .trim(),
+}
 
 module.exports.meta = {
-  title: 'The Tallest Man on Earth – Stay Home With Me',
+  title: 'The Tallest Man on Earth – #StayHome #WithMe',
   description: 'All The Tallest Man on Earth Stay Home With Me songs',
   id: 'tmoe',
   playlistId: 'PLsqIAvvqdduhw1f7RVxdcCmaCm5Zy7Osc',
