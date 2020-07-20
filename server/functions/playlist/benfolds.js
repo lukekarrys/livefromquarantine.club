9589bf56eed5af5571ab0d82f22be9221461e343
Luke Karrys <luke@lukekarrys.com>
2020-07-19 20:30:14 -0700

    Update benfolds and bengibbard titles

--- server/functions/playlist/benfolds.js
+++ server/functions/playlist/benfolds.js
@@ -1,4 +1,12 @@
 module.exports = {
+  parsers: {
+    title: (title) =>
+      title
+        .split('-')[0]
+        .replace(/\bw Ben Folds\b/gi, '')
+        .replace(/\bBen Folds\b/gi, '')
+        .replace(/\bFolds\b/gi, '')
+  },
   playlistId: 'PLG507gy2-Kp8Vj66jnxn1AA0XFr1L_QXy',
   id: 'benfolds',
   meta: {
