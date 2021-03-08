6c48d9de8e98bdc207a3420dfd92777b84421f2d
Luke Karrys <lukekarrys@gmail.com>
2021-03-08 08:17:11 -0700

    dont use env for import just try both

--- server/api/import.ts
+++ server/api/import.ts
@@ -1,13 +1,24 @@
 import path from 'path'
-import detectTSNode from 'detect-ts-node'
 
-const ext = detectTSNode ? 'ts' : 'js'
+const tryExts = ['ts', 'js']
+
+const getDefault = <T>(p: string) =>
+  import(p).then<T>((r: { default: T }) => r.default)
 
 // import files normally and return default export
-// if no ext is provided lookup ts or js based on env
-const importEnv = <T>(p: string): Promise<T> =>
-  import(path.extname(p) ? p : `${p}.${ext}`).then(
-    (r: { default: T }) => r.default
-  )
+// if no ext is provided lookup ts, then js
+const importEnv = async <T>(p: string): Promise<T> => {
+  const hasExt = path.extname(p)
+
+  if (hasExt) {
+    return getDefault<T>(p)
+  }
+
+  try {
+    return getDefault<T>(`${p}.${tryExts[0]}`)
+  } catch (e) {
+    return await getDefault<T>(`${p}.${tryExts[1]}`)
+  }
+}
 
 export default importEnv
