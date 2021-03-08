1255757d211d56a17f66c87e465ec3c3cade46e5
Luke Karrys <lukekarrys@gmail.com>
2021-03-08 08:31:28 -0700

    update lint staged config

--- client/lint-staged.config.js
+++ client/lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   '*.{css,md,json}': ['prettier --write'],
   '*.{js,jsx,ts,tsx}': [`prettier --write`, `eslint --fix`],
-  // Make this key different so it runs on the whole project instead
-  // of just staged files
-  '*.{js,jsx,ts,tsx,FORJESTTESTS}': () => `jest --bail`,
-  '*.{js,jsx,ts,tsx,TYPESCRIPT}': () => 'tsc -p tsconfig.json --noEmit',
+  // Make this key different so it runs for all files
+  // instead of just staged files
+  '*.{js,jsx,ts,tsx,FOR_TESTING}': () => `jest --bail`,
+  '*.{js,jsx,ts,tsx,FOR_TYPESCRIPT}': () => 'tsc -p tsconfig.json --noEmit',
 }
--- server/lint-staged.config.js
+++ server/lint-staged.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   '*.{css,md,json}': ['prettier --write'],
   '*.{js,ts}': [`prettier --write`, `eslint --fix`],
-  // Make this key different so it runs on the project instead
-  // of just staged files
-  '*.{js,ts,testingfiles}': () => 'ava',
-  // Make this key different so it runs on the project instead
-  // of just staged files
-  '*.{js,ts,typescript}': () => 'tsc -p tsconfig.json --noEmit',
+  // Make this key different so it runs for all files
+  // instead of just staged files
+  '*.{js,ts,FOR_TESTING}': () => 'ava',
+  '*.{js,ts,FOR_TYPESCRIPT}': () => 'tsc -p tsconfig.json --noEmit',
 }
