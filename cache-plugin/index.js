8b85a5f15c1c7d40ae81b21dc724c3422ddc86ce
Luke Karrys <lukekarrys@gmail.com>
2021-03-12 13:03:35 -0700

    cache plugin

--- cache-plugin/index.js
+++ cache-plugin/index.js
@@ -1,19 +1,31 @@
-const cache = require('@netlify/cache-utils')
 const path = require('path')
 
 const directories = ['.', 'client', 'server']
 
 const x = {
-  async onPreBuild() {
-    await cache.restore(directories.map((d) => path.join(d, 'node_modules')))
+  async onPreBuild({ utils: { cache } }) {
+    const preBuildDirectories = directories.map((d) =>
+      path.join(d, 'node_modules')
+    )
+
+    console.log('onPreBuild:', preBuildDirectories)
+
+    await cache.restore(preBuildDirectories)
   },
-  async onPostBuild() {
+  async onPostBuild({ utils: { cache } }) {
     await Promise.all(
-      directories.map((d) =>
-        cache.save(path.join(d, 'node_modules'), {
-          digests: [path.join(d, 'package-lock.json')],
-        })
-      )
+      directories.map((d) => {
+        const saveParameters = [
+          path.join(d, 'node_modules'),
+          {
+            digests: [path.join(d, 'package-lock.json')],
+          },
+        ]
+
+        console.log('onPostBuild:', saveParameters)
+
+        cache.save(...saveParameters)
+      })
     )
   },
 }
