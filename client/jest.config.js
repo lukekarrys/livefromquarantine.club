e6cb451397531c3bf6bca327604f5fb8b93b0aa9
Luke Karrys <lukekarrys@gmail.com>
2021-03-28 09:58:54 -0700

    use npm workspaces (#233)

--- client/jest.config.js
+++ client/jest.config.js
@@ -7,14 +7,14 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testURL: 'http://localhost:8080',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  moduleDirectories: ['node_modules'],
+  moduleDirectories: ['../node_modules'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/tests/__mocks__/fileMock.ts',
     '\\.(css|less|scss)$': 'identity-obj-proxy',
     '^./style$': 'identity-obj-proxy',
-    '^preact$': '<rootDir>/node_modules/preact/dist/preact.min.js',
+    '^preact$': '<rootDir>/../node_modules/preact/dist/preact.min.js',
     '^react$': 'preact/compat',
     '^react-dom$': 'preact/compat',
   },
